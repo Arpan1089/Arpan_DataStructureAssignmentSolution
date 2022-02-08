7c66c93cd604497d8368b0b9cbe41978e8d7a5c9
arpan1089 <git>
2022-02-08 16:29:59 +0530

    indentation

--- dataStructureAssignmentSolution/src/com/skyscraper/Driver.java
+++ dataStructureAssignmentSolution/src/com/skyscraper/Driver.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) {
-        System.out.println("Enter the total no of floors in the building");
-        Scanner scanner = new Scanner(System.in);
-        int totalFloors = scanner.nextInt();
-        SkyscraperConstruction skyscraperConstruction = new SkyscraperConstruction(totalFloors);
-        skyscraperConstruction.inputFloorDeliveryByDay(scanner);
-        skyscraperConstruction.processAndShowOrderOfConstruction();
-    }
+		System.out.println("Enter the total no of floors in the building");
+		Scanner scanner = new Scanner(System.in);
+		int totalFloors = scanner.nextInt();
+		SkyscraperConstruction skyscraperConstruction = new SkyscraperConstruction(totalFloors);
+		skyscraperConstruction.inputFloorDeliveryByDay(scanner);
+		skyscraperConstruction.processAndShowOrderOfConstruction();
+	}
 }
